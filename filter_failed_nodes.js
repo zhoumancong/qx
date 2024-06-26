f402263591f8c66c600883f382794685948c7afb
zhoumancong <mc_zh@qq.com>
2024-06-26 11:30:02 +0800

    Create filter_failed_nodes.js

---
+++ filter_failed_nodes.js
@@ -0,0 +1,19 @@
+// filter_failed_nodes.js
+
+let nodes = $surge.selectGroup($environment.policy);
+let validNodes = [];
+
+for (let node of nodes) {
+    $httpClient.get('http://www.google.com/generate_204', { node: node }, (error, response, data) => {
+        if (!error && response.statusCode == 204) {
+            validNodes.push(node);
+        }
+        if (validNodes.length == nodes.length) {
+            $done(validNodes);
+        }
+    });
+}
+
+setTimeout(() => {
+    $done(validNodes);
+}, 10000); // 超时时间，10秒后返回已筛选的有效节点
