8227a20d4382b0c7d6e68196387b13fc3629d529
zhoumancong <mc_zh@qq.com>
2024-06-26 11:40:33 +0800

    Update filter_failed_nodes.js

--- filter_failed_nodes.js
+++ filter_failed_nodes.js
@@ -1,20 +1,37 @@
-#连接测试
 // filter_failed_nodes.js
 
-let nodes = $surge.selectGroup($environment.policy);
-let validNodes = [];
+// 测试网址，你可以根据需要更改
+const testUrl = "http://www.google.com/generate_204";
+const timeout = 5000;  // 设置超时时间（毫秒）
+const nodes = $resource.getPolicyList("YOUR_POLICY_NAME"); // 替换为你的策略组名称
 
-for (let node of nodes) {
-    $httpClient.get('http://www.google.com/generate_204', { node: node }, (error, response, data) => {
-        if (!error && response.statusCode == 204) {
+let validNodes = [];
+let testedNodes = 0;
+
+function testNode(node) {
+    $httpClient.get(testUrl, { node: node }, (error, response, data) => {
+        testedNodes++;
+        if (!error && response.statusCode === 204) {
             validNodes.push(node);
         }
-        if (validNodes.length == nodes.length) {
-            $done(validNodes);
+
+        if (testedNodes === nodes.length) {
+            applyPolicy(validNodes);
         }
     });
 }
 
+function applyPolicy(validNodes) {
+    $surge.setSelectGroupPolicy("YOUR_POLICY_NAME", validNodes);
+    $done();
+}
+
+for (let node of nodes) {
+    testNode(node);
+}
+
 setTimeout(() => {
-    $done(validNodes);
-}, 10000); // 超时时间，10秒后返回已筛选的有效节点
+    if (testedNodes < nodes.length) {
+        applyPolicy(validNodes);
+    }
+}, timeout);
